9dd2dfb50aa6c78aaf67ced9f08a5062b94d555b
Ross Rothenstine <rossr@unity3d.com>
2023-11-27 09:33:02 -0800

    go: add utility script to import required go

---
+++ scripts/up-goutils.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+go install golang.org/x/tools/gopls@latest
+go install mvdan.cc/gofumpt@latest
+go install golang.org/x/tools/cmd/goimports@latest
+
--- scripts/up-macos.sh
+++ scripts/up-macos.sh
@@ -4,7 +4,7 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install requisite packages.
-brew install git zsh tmux neovim
+brew install git zsh tmux neovim go 
 
 this_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ./$(this_dir)/up-unix.sh
