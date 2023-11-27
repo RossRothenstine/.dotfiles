8a157d512585b6ec11f0c9e2bd99d210b20207cf
Ross Rothenstine <rossr@unity3d.com>
2023-11-27 15:06:29 -0800

    git: more configs

--- scripts/up-macos.sh
+++ scripts/up-macos.sh
@@ -4,7 +4,12 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install requisite packages.
-brew install llvm gcc git zsh tmux neovim go fzf
+brew install diff-so-fancy llvm gcc git zsh tmux neovim go fzf
 
 this_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ./$(this_dir)/up-unix.sh
+
+git config --global core.editor "nvim"
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RF"
+git config --global interactive.diffFilter "diff-so-fancy --patch"
+
