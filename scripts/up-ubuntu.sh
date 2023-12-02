7f1409b6d491390aa6757afd61db569ae54451ca
Ross Rothenstine <rossrothenstine@gmail.com>
2023-12-02 11:54:38 -0800

    ubuntu: add some additional setup scripts

--- scripts/up-ubuntu.sh
+++ scripts/up-ubuntu.sh
@@ -4,4 +4,15 @@
 apt-get install -y git \
 	zsh
 
+# Install neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage && \
+	chmod u+x nvim.appimage && \
+	mv nvim.appimage ~/.local/bin/nvim
+
+# Install oh-my-zsh.
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# Install zsh-autosuggestions.
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 $(dirname "${BASH_SOURCE[0]}")/up-unix.sh
